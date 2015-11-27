ce3d24dcddcabbfaf6cba4c3467c507509ffed25
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-11-27 12:11:53 +0000

    [makeotflib.__init__] add function calling makeotflib main with arguments

--- makeotflib/__init__.py
+++ makeotflib/__init__.py
@@ -1,1 +1,18 @@
-from ._makeotflib import *
+from ._makeotflib_cffi import ffi, lib
+import sys
+from fontTools.misc.py23 import tobytes, basestring
+
+
+def makeotfexe(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    else:
+        if isinstance(args, basestring):
+            import shlex
+            args = shlex.split(args)
+    progname = ffi.new("char[]", b"makeotflib")
+    argv = [progname]
+    for arg in args:
+        argv.append(ffi.new("char[]", tobytes(arg)))
+    argc = len(argv)
+    return lib.main(argc, argv)
