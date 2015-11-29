91bf2c1bf732887bc8d8dfbb68e54be5bdb1e3a6
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-11-29 12:09:32 +0000

    build_makeotflib.py: add support for building C libraries to link with cffi module; import build info and custom_build_clib from setup.py

--- build_makeotflib.py
+++ build_makeotflib.py
@@ -3,33 +3,12 @@ import os
 import sys
 import sysconfig
 import shutil
-import errno
-
 
 CURR_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
-AFDKO = os.path.join(CURR_DIR, 'afdko')
-
-PUBLIC_LIB = os.path.join(AFDKO, 'FDK', 'Tools', 'Programs', 'public', 'lib')
-
-MAKEOTF_ROOT = os.path.join(AFDKO, 'FDK', 'Tools', 'Programs', 'makeotf')
-MAKEOTF_LIB = os.path.join(MAKEOTF_ROOT, 'makeotf_lib')
-MAKEOTF_SOURCE = os.path.join(MAKEOTF_ROOT, 'source')
-
-BUILD_TEMP = os.path.join(
-    CURR_DIR, "build", "temp.{platform}-{version[0]}.{version[1]}".format(
-        platform=sysconfig.get_platform(),
-        version=sys.version_info))
-
-
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
+sys.path.insert(0, CURR_DIR)
+from setup import (
+    custom_build_clib, LIBRARIES, PUBLIC_LIB, MAKEOTF_LIB, MAKEOTF_SOURCE)
 
 
 ffi = cffi.FFI()
@@ -42,7 +21,7 @@ with open(os.path.join(MAKEOTF_SOURCE, "main.c"), 'r') as f:
     source = f.read()
 
 ffi.set_source(
-    '_makeotflib_cffi',
+    'makeotflib._makeotflib_cffi',
     source,
     source_extension='.c',
     define_macros=[("MAKEOTFLIB_EXPORTS", "1")],
@@ -51,32 +30,86 @@ ffi.set_source(
         os.path.join(MAKEOTF_SOURCE, "cbpriv.c"),
         os.path.join(MAKEOTF_SOURCE, "fcdb.c"),
         os.path.join(MAKEOTF_SOURCE, "file.c"),
+    ] + ([
+        os.path.join(MAKEOTF_SOURCE, "Win32", "Win.c"),
+    ] if os.name == "nt" else [
         os.path.join(MAKEOTF_SOURCE, "mac", "mac.c"),
-    ],
+    ]),
     include_dirs=[
         os.path.join(MAKEOTF_SOURCE, "include_files"),
         os.path.join(MAKEOTF_LIB, "api"),
         os.path.join(MAKEOTF_LIB, "resource"),
         os.path.join(PUBLIC_LIB, "api"),
     ],
-    extra_objects=[
-        os.path.join(BUILD_TEMP, "libctutil.a"),
-        os.path.join(BUILD_TEMP, "libdynarr.a"),
-        os.path.join(BUILD_TEMP, "libhotconv.a"),
-        os.path.join(BUILD_TEMP, "libpstoken.a"),
-        os.path.join(BUILD_TEMP, "libtypecomp.a"),
-        os.path.join(BUILD_TEMP, "libcffread.a"),
-    ],
 )
 
 
-if __name__ == '__main__':
-    # compile extension module to build/temp-* folder
-    mkdir_p(BUILD_TEMP)
-    ext_path = ffi.compile(BUILD_TEMP)
+def get_platform_build_dir(subdir_prefix='temp', base_dir=""):
+    return os.path.join(
+        base_dir, "build",
+        "{prefix}.{platform}-{version[0]}.{version[1]}".format(
+            prefix=subdir_prefix,
+            platform=sysconfig.get_platform(),
+            version=sys.version_info))
 
-    # copy compiled module inside package dir
-    dest = os.path.join(CURR_DIR, 'makeotflib', os.path.basename(ext_path))
+
+def build_c_libraries(tmpdir, libraries):
+    from distutils.core import Distribution
+    import distutils.errors
+    from cffi.ffiplatform import VerificationError
+    from distutils.ccompiler import new_compiler
+
+    dist = Distribution()
+    dist.parse_config_files()
+
+    dist.cmdclass['build_clib'] = custom_build_clib
+
+    cmd_obj = dist.get_command_obj('build_clib')
+    compiler = new_compiler(cmd_obj.compiler)
+    libraries = [(name, info) for name, info in libraries
+                 if not os.path.exists(
+                     compiler.library_filename(name, output_dir=tmpdir))]
+    if not libraries:
+        return
+
+    dist.libraries = libraries
+
+    options = dist.get_option_dict('build_clib')
+    options['force'] = ('ffiplatform', True)
+    options['build_clib'] = ('ffiplatform', tmpdir)
+    options['build_temp'] = ('ffiplatform', tmpdir)
+
+    try:
+        dist.run_command('build_clib')
+    except (distutils.errors.CompileError,
+            distutils.errors.LinkError) as e:
+        raise VerificationError('%s: %s' % (e.__class__.__name__, e))
+
+
+if __name__ == '__main__':
+    from distutils.dir_util import mkpath
+
+    build_temp = get_platform_build_dir('temp', base_dir=CURR_DIR)
+    mkpath(build_temp)
+
+    # compile C libraries if missing from build temp
+    build_c_libraries(build_temp, LIBRARIES)
+
+    # add libraries to cffi module's build info
+    module_name, source, source_extension, kwds = ffi._assigned_source
+    del ffi._assigned_source
+    kwds['libraries'] = [lib_name for lib_name, build_info in LIBRARIES]
+    kwds['library_dirs'] = [build_temp]
+    ffi.set_source(module_name, source, source_extension, **kwds)
+
+    # compile cffi module
+    ext_path = ffi.compile(build_temp)
+
+    # copy compiled module to build_lib's package dir
+    build_lib = get_platform_build_dir('lib', base_dir=CURR_DIR)
+    package_lib = os.path.join(build_lib, 'makeotflib')
+    mkpath(package_lib)
+    dest = os.path.join(package_lib, os.path.basename(ext_path))
     if os.path.exists(dest):
         os.remove(dest)
     shutil.copy(ext_path, dest)
