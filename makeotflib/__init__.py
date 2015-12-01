a4303626d30e157315c105b973d9863710b9d9e0
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-12-01 12:17:49 +0000

    __init__.py: add wrapper for makeotflib's C main function

--- makeotflib/__init__.py
+++ makeotflib/__init__.py
@@ -1,1 +1,19 @@
-from ._makeotflib import *
+from __future__ import (
+    print_function, division, absolute_import, unicode_literals)
+import sys
+from . import _makeotflib as lib
+from .py23 import tobytes, basestring
+
+
+def main(args=None):
+    if args is None:
+        # if no arguments, read argument list from console
+        args = sys.argv[1:]
+    else:
+        if isinstance(args, basestring):
+            # if string, split into separate arguments
+            import shlex
+            args = shlex.split(args)
+    # convert arguments to bytes
+    args = [tobytes(a) for a in args]
+    return lib.main(args)
